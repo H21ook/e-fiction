8af173bdb0a2309aef6307ae24700ec6bf7dfcfd
Hishigbayr <hishigbayr_3@yahoo.com>
2019-08-28 08:27:49 +0800

    deploy test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>E Fiction Test</h1>
+        <h1>E Fiction</h1>
       </header>
     </div>
   );
